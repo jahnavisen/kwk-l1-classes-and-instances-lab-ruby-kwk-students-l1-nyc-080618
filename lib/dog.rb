578caefeded040f893c5e09774cda3b9277a5b67
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-09 19:00:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,8 @@
 # dog.rb
 class Dog 
-  def initialize(name)
-    @name = name 
-  end 
-  def name 
-    return @name 
-  end 
+  fido = Dog.new 
+  lassie  = Dog.new 
+  snoopy  = Dog.new 
 end 
-
  
     
